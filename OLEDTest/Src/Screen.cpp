1f5986b592eb0fc3d3c044ebf4250fb07777d9c9
Ivan Melechko <melechko@trueconf.com>
2021-09-20 12:56:45 +0300

    const fix

--- OLEDTest/Src/Screen.cpp
+++ OLEDTest/Src/Screen.cpp
@@ -10,6 +10,13 @@
 #include "ssd1306.h"
 #include "TM1638.h"
 
+//----------------------------------
+char cMoonShine[]="MoonShine";
+char cVersion[]="v0.1";
+char cPressAnyKey[]=     "Press any key";
+char cPressAnyKeyBlank[]="             ";
+//----------------------------------
+
 CStartScreen::~CStartScreen(){
 
 };
@@ -21,11 +28,9 @@ void CStartScreen::Init(){
 	SSD1306_DrawRectangle(0, 0, 127, 63, SSD1306_COLOR_WHITE);
 	SSD1306_DrawFilledRectangle(8, 8, 127-16, 63-16, SSD1306_COLOR_WHITE);
 	SSD1306_GotoXY (16,9);
-	SSD1306_Puts ("MoonShine", &Font_11x18, SSD1306_COLOR_BLACK);
+	SSD1306_Puts (cMoonShine, &Font_11x18, SSD1306_COLOR_BLACK);
 	SSD1306_GotoXY (44,27);
-	SSD1306_Puts ("v0.1", &Font_11x18, SSD1306_COLOR_BLACK);
-	  //SSD1306_GotoXY (20,45);
-	 // SSD1306_Puts ("Press any key", &Font_7x10, 0);
+	SSD1306_Puts (cVersion, &Font_11x18, SSD1306_COLOR_BLACK);
 	SSD1306_UpdateScreen();
     for(int i=0;i<8;i++)
     	TM1638_Char(i,digitToSegment[0x10]);
@@ -36,12 +41,12 @@ void CStartScreen::Update() {
 	m_count++;
 	if (m_count == 32) {
 		SSD1306_GotoXY(20, 45);
-		SSD1306_Puts("Press any key", &Font_7x10, SSD1306_COLOR_BLACK);
+		SSD1306_Puts(cPressAnyKey, &Font_7x10, SSD1306_COLOR_BLACK);
 		SSD1306_UpdateScreen();
 	} else {
 		if (m_count >= 64) {
 			SSD1306_GotoXY(20, 45);
-			SSD1306_Puts("             ", &Font_7x10, SSD1306_COLOR_BLACK);
+			SSD1306_Puts(cPressAnyKeyBlank, &Font_7x10, SSD1306_COLOR_BLACK);
 			SSD1306_UpdateScreen();
 			m_count = 0;
 		}
